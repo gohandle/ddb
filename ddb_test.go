8698c250de5eafb5bb231c67e7874cf9a9d1ddbf
advanderveer <advanderveer@gmail.com>
2020-12-01 14:52:05 +0100

    added e2e test

--- ddb_test.go
+++ ddb_test.go
@@ -1,16 +1,20 @@
 package ddb
 
 import (
+	"context"
 	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	e "github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 // withLocalDB will run local Dynamodb for the duration of the test while creating any tables
@@ -54,4 +58,188 @@ func withLocalDB(tb testing.TB, tbls ...*dynamodb.CreateTableInput) (ddb *dynamo
 	}
 
 	return
+}
+
+// table1 describes a simple table that just has a string pk
+type table1 string
+
+func (tbl table1) createInput() *dynamodb.CreateTableInput {
+	return (&dynamodb.CreateTableInput{}).
+		SetTableName(string(tbl)).
+		SetProvisionedThroughput((&dynamodb.ProvisionedThroughput{}).
+			SetReadCapacityUnits(1).
+			SetWriteCapacityUnits(1)).
+		SetAttributeDefinitions(
+			[]*dynamodb.AttributeDefinition{
+				(&dynamodb.AttributeDefinition{}).
+					SetAttributeName("pk").SetAttributeType("S"),
+			}).
+		SetKeySchema(
+			[]*dynamodb.KeySchemaElement{
+				(&dynamodb.KeySchemaElement{}).
+					SetAttributeName("pk").
+					SetKeyType("HASH"),
+			})
+}
+
+type table1Item struct {
+	PK string `dynamodbav:"pk"`
+	F1 string `dynamodbav:"f1"`
+}
+
+func (table1Item) Keys() (pk, sk string) {
+	return "pk", ""
+}
+
+type table1Entity struct {
+	ID   int
+	Name string
+}
+
+func (e *table1Entity) FromItem(it Item) {
+	e.ID, _ = strconv.Atoi(it.(*table1Item).PK[1:])
+	e.Name = it.(*table1Item).F1
+}
+
+func (e table1Entity) Item() Item {
+	return &table1Item{
+		PK: "e" + strconv.Itoa(e.ID),
+		F1: e.Name,
+	}
+}
+
+func (tbl table1) simpleScan() (b e.Builder, s dynamodb.ScanInput) {
+	s.SetTableName(string(tbl))
+	s.SetLimit(2)
+	return
+}
+
+func (tbl table1) simpleGet1(id int) (b e.Builder, g dynamodb.Get, k Itemizer) {
+	g.SetTableName(string(tbl))
+	return b, g, &table1Entity{ID: id}
+}
+
+func (tbl table1) simpleUpd1(id int, newName string) (b e.Builder, u dynamodb.Update, k Itemizer) {
+	u.SetTableName(string(tbl))
+	return b.WithUpdate(
+		e.Set(e.Name("f1"), e.Value(newName)),
+	), u, &table1Entity{ID: id}
+}
+
+func (tbl table1) simpleQry1(id int) (b e.Builder, q dynamodb.QueryInput) {
+	pk := (&table1Entity{ID: id}).Item().(*table1Item).PK
+
+	q.SetTableName(string(tbl))
+	return b.WithKeyCondition(
+		e.Key("pk").Equal(e.Value(pk)),
+	), q
+}
+
+func (tbl table1) simpleDel1(id int) (b e.Builder, p dynamodb.Delete, k Itemizer) {
+	p.SetTableName(string(tbl))
+	return b, p, &table1Entity{ID: id}
+}
+
+func (tbl table1) simplePut1(e *table1Entity) (b e.Builder, p dynamodb.Put, it Itemizer) {
+	p.SetTableName(string(tbl))
+	return b, p, e
+}
+
+func TestSimpleEnd2End(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	tbl := table1(t.Name())
+	ddb := withLocalDB(t, tbl.createInput())
+
+	t.Run("put 10", func(t *testing.T) {
+		for i := 0; i < 10; i++ {
+			e := &table1Entity{i, "name-" + strconv.Itoa(i)}
+			if _, err := Put(tbl.simplePut1(e)).Run(ctx, ddb); err != nil {
+				t.Fatalf("got: %v", err)
+			}
+		}
+
+		t.Run("delete 5", func(t *testing.T) {
+			for i := 0; i < 5; i++ {
+				if _, err := Delete(tbl.simpleDel1(i)).Run(ctx, ddb); err != nil {
+					t.Fatalf("got: %v", err)
+				}
+			}
+
+			t.Run("update nr 6", func(t *testing.T) {
+				if _, err := Update(tbl.simpleUpd1(6, "foo")).Run(ctx, ddb); err != nil {
+					t.Fatalf("got: %v", err)
+				}
+
+				t.Run("query nr 6", func(t *testing.T) {
+					r, err := Query(tbl.simpleQry1(6)).Run(ctx, ddb)
+					if err != nil {
+						t.Fatalf("got: %v", err)
+					}
+
+					for r.Next() {
+						var e table1Entity
+						if err := r.Scan(&e); err != nil {
+							t.Fatalf("got: %v", err)
+						}
+
+						if e.ID != 6 || e.Name != "foo" {
+							t.Fatalf("got: %v", e)
+						}
+					}
+
+					if err = r.Err(); err != nil {
+						t.Fatalf("got: %v", err)
+					}
+				})
+
+				t.Run("get nr 6", func(t *testing.T) {
+					r, err := Get(tbl.simpleGet1(6)).Run(ctx, ddb)
+					if err != nil {
+						t.Fatalf("got: %v", err)
+					}
+
+					for r.Next() {
+						var e table1Entity
+						if err := r.Scan(&e); err != nil {
+							t.Fatalf("got: %v", err)
+						}
+
+						if e.ID != 6 || e.Name != "foo" {
+							t.Fatalf("got: %v", e)
+						}
+					}
+
+					if err = r.Err(); err != nil {
+						t.Fatalf("got: %v", err)
+					}
+				})
+
+				t.Run("scan all", func(t *testing.T) {
+					r, err := Scan(tbl.simpleScan()).Run(ctx, ddb)
+					if err != nil {
+						t.Fatalf("got: %v", err)
+					}
+
+					var names []string
+					for r.Next() {
+						var e table1Entity
+						if err := r.Scan(&e); err != nil {
+							t.Fatalf("got: %v", err)
+						}
+						names = append(names, e.Name)
+					}
+
+					if err = r.Err(); err != nil {
+						t.Fatalf("got: %v", err)
+					}
+
+					if act := strings.Join(names, ","); act != "name-9,name-7,name-5,name-8,foo" {
+						t.Fatalf("got: %v", act)
+					}
+				})
+			})
+		})
+	})
 }
---
+++ scan.go
@@ -0,0 +1,97 @@
+package ddb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
+// Scanner holds a DynamoDB query
+type Scanner struct {
+	res *scanResult
+	eb  expression.Builder
+}
+
+// Scan sets up a scanner that can be run to fetch
+func Scan(b expression.Builder, in dynamodb.ScanInput) (q *Scanner) {
+	q = new(Scanner)
+	q.res = &scanResult{pos: -1}
+	q.res.in = &in
+	q.eb = b
+	return
+}
+
+// Run will return a Query result for iteration
+func (q *Scanner) Run(ctx context.Context, ddb Dynamo) (r Result, err error) {
+	expr, err := exprBuild(q.eb)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build expression(s): %w", err)
+	}
+
+	q.res.ddb = ddb
+	q.res.ctx = ctx
+	q.res.in.FilterExpression = expr.Filter()
+	q.res.in.ProjectionExpression = expr.Projection()
+	q.res.in.ExpressionAttributeNames = expr.Names()
+	q.res.in.ExpressionAttributeValues = expr.Values()
+	return q.res, nil
+}
+
+// scanResult is a result that is returned when a query operation
+// is run. It will automatically get more pages as the user scans
+// through the results.
+type scanResult struct {
+	ctx context.Context
+	in  *dynamodb.ScanInput
+	out *dynamodb.ScanOutput
+	ddb Dynamo
+	err error
+	pos int
+}
+
+func (c *scanResult) Err() error {
+	return c.err
+}
+
+func (c *scanResult) Next() bool {
+
+	// we got some query output, start with pos iteration
+	c.pos++
+	if c.out != nil && c.pos >= len(c.out.Items) {
+		if c.out.LastEvaluatedKey == nil {
+			return false // fully done
+		}
+
+		// else we prep the cursor for a new query
+		if c.out.LastEvaluatedKey != nil {
+			c.pos = 0
+			c.in.ExclusiveStartKey = c.out.LastEvaluatedKey
+			c.out = nil
+		}
+	}
+
+	// no out, must be the first time it is queried
+	if c.out == nil {
+		if c.out, c.err = c.ddb.ScanWithContext(c.ctx, c.in); c.err != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (c *scanResult) Scan(v interface {
+	Itemizer
+	Deitemizer
+}) (err error) {
+	it := v.Item()
+	if err = dynamodbattribute.UnmarshalMap(c.out.Items[c.pos], it); err != nil {
+		return
+	}
+
+	v.FromItem(it)
+	return
+}
